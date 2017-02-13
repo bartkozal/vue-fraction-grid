3b89494a1e97991d488f63b10bb109997a609700
Bartlomiej Kozal <bkzl@me.com>
2017-02-13 01:28:51 +0100

    Scaffold components

--- vue-fraction-grid.js
+++ vue-fraction-grid.js
@@ -1,1 +1,13 @@
-// TODO
+import Container from './components/Container'
+import Grid from './components/Grid'
+import GridItem from './components/GridItem'
+
+const VueFractionGrid = {
+  install (Vue, options) {
+    Vue.component(Container.name, Container)
+    Vue.component(Grid.name, Grid)
+    Vue.component(GridItem.name, GridItem)
+  }
+}
+
+export default VueFractionGrid
