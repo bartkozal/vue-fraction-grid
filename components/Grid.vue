b93a0321f571701ab7ad25cb9fd818a1f72f2a60
Bartlomiej Kozal <bkzl@me.com>
2017-02-17 21:08:55 +0100

    Base Grid component

--- components/Grid.vue
+++ components/Grid.vue
@@ -1,8 +1,32 @@
+<template>
+  <div class="vfg-grid" :style="styleObject">
+    <slot></slot>
+  </div>
+</template>
+
 <script>
 import initConfig from '../utils/init-config'
 
 export default {
   name: 'grid',
-  mixins: [initConfig]
+  mixins: [initConfig],
+  computed: {
+    styleObject () {
+      return {
+        marginRight: this.config.gutter / -2,
+        marginLeft: this.config.gutter / -2
+      }
+    }
+  }
 }
 </script>
+
+<style scoped>
+.vfg-grid {
+  box-sizing: border-box;
+  display: flex;
+  flex: 0 1 auto;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+</style>
