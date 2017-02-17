cbd64d4bbc7fb71b02e74a65b12e9040c3ff2e37
Bartlomiej Kozal <bkzl@me.com>
2017-02-17 21:09:13 +0100

    Update demo

--- demo/Demo.vue
+++ demo/Demo.vue
@@ -1,24 +1,70 @@
 <template>
   <div>
-    <h1>vue-fraction-grid</h1>
+    <section data-name="vue-fraction-grid">
+      <h1>vue-fraction-grid</h1>
 
-    <h2>Container</h2>
+      <container>
+        <grid vertical="middle" compact="stack">
+          <grid-item size="1/6" compact="0/1">
+            1/6, compact: 0/1
+          </grid-item>
 
-    <container class="bg">
-      Default
-    </container>
+          <grid-item size="3/6" compact="1/1">
+            3/6, compact: 1/1
+          </grid-item>
 
-    <hr>
+          <grid-item size="2/6" compact="1/1">
+            2/6, compact: 1/1
+          </grid-item>
+        </grid>
+      </container>
+    </section>
 
-    <container class="bg" width="720px">
-      720px
-    </container>
+    <section data-name="container">
+      <h2>Container</h2>
+
+      <container>
+        Default
+      </container>
+
+      <hr>
+
+      <container width="720px">
+        720px
+      </container>
+    </section>
+
+    <section data-name="grid">
+      <h2>Grid</h2>
+
+      <grid>
+        <grid-item size="1/3">
+          1/3
+        </grid-item>
+
+        <grid-item size="2/3">
+          2/3
+        </grid-item>
+      </grid>
+    </section>
   </div>
 </template>
 
 <style>
-/* #fafafa #a2d5f2 #07689f #ff7e67 */
-.bg {
+.vfg-container {
   background-color: #a2d5f2;
+  text-align: center;
+}
+
+.vfg-grid {
+  background-color: #07689f;
+  color: #fafafa;
+  text-align: center;
+}
+
+.vfg-grid-item {
+  background-color: #ff7e67;
+  color: #fafafa;
+  text-align: center;
 }
 </style>
